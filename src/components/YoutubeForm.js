35cfce2f619821e8069f9aaeba8d4afe734ae234
Alpha2Chris14 <onyekachristian14@gmail.com>
2022-10-02 19:52:55 +0100

    added onblur to only show validation when field loses focus

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -35,20 +35,23 @@ function YoutubeForm(){
             <form onSubmit={formik.handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name}/>
-                    {formik.errors.name ? <div className="error">{formik.errors.name}</div> :null }
+                    <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur}/>
+                    {formik.touched.name
+                     && formik.errors.name ? <div className="error">{formik.errors.name}</div> :null }
                 </div>
 
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email}/>
-                    {formik.errors.email ? <div className="error">{formik.errors.email}</div> :null }
+                    <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur}/>
+                    {formik.touched.email
+                     && formik.errors.email ? <div className="error">{formik.errors.email}</div> :null }
                 </div>
 
                 <div className="form-control">
                     <label htmlFor="channel">Channel</label>
-                    <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel}/>
-                    {formik.errors.channel ? <div className="error">{formik.errors.channel}</div> :null }
+                    <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} onBlur={formik.handleBlur}/>
+                    {formik.touched.channel
+                     && formik.errors.channel ? <div className="error">{formik.errors.channel}</div> :null }
                 </div>
 
                 <button>Submit</button>
